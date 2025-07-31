c4219674d27f330bd0e2500aa873619443f9778e
Alfred-Kang <alfredkangjr@gmail.com>
2025-07-31 16:37:33 +0800

    feat: imported videos, added basic main menu and video playing functionality

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour
 {
@@ -8,18 +9,51 @@ public class CameraController : MonoBehaviour
     private float yaw = 0f;
     private float pitch = 0f;
 
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    [Header("UI Interaction")]
+    public GraphicRaycaster raycaster;
+    public EventSystem eventSystem;
+    public Camera mainCamera;
 
     void Update()
     {
+        HandleLook();
+
+        if (Cursor.lockState == CursorLockMode.Locked && Input.GetMouseButtonDown(0))
+        {
+            TryClickUI();
+        }
+    }
+
+    void HandleLook()
+    {
+        if (Cursor.lockState != CursorLockMode.Locked)
+            return;
+
         yaw += Input.GetAxis("Mouse X") * sensitivity;
         pitch -= Input.GetAxis("Mouse Y") * sensitivity;
         pitch = Mathf.Clamp(pitch, -90f, 90f);
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+
+    void TryClickUI()
+    {
+        PointerEventData pointerData = new PointerEventData(eventSystem);
+        pointerData.position = new Vector2(Screen.width / 2, Screen.height / 2);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(pointerData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            Debug.Log(result.gameObject.name);
+            Button button = result.gameObject.GetComponent<Button>();
+            if (button != null)
+            {
+                button.onClick.Invoke();
+                Debug.Log("Clicked on: " + result.gameObject.name);
+                break;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Video;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject crosshair;
+    public GameObject mainMenuUI;
+    public Canvas HUDCanvas;
+    public GameObject outTouchpoints;
+    public GameObject inTouchpoints;
+
+    public GameObject videoSphere;
+    public VideoPlayer videoPlayer;
+    
+    public Material introVideoMaterial;
+    public Material outStillImageMaterial;
+    public Material inStillImageMaterial;
+
+    private MeshRenderer sphereRenderer;
+
+    void Start()
+    {
+        sphereRenderer = videoSphere.GetComponent<MeshRenderer>();
+
+        videoSphere.SetActive(false);
+        videoPlayer.loopPointReached += OnVideoEnd;
+
+        ShowMainMenu();
+    }
+
+    public void ShowMainMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        crosshair.SetActive(false);
+        outTouchpoints.SetActive(false);
+        inTouchpoints.SetActive(false);
+        mainMenuUI.SetActive(true);
+        videoSphere.SetActive(false);
+    }
+
+    public void StartSimulation()
+    {
+        crosshair.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        mainMenuUI.SetActive(false);
+        outTouchpoints.SetActive(false);
+        videoSphere.SetActive(true);
+
+        sphereRenderer.material = introVideoMaterial;
+        Play360Video();
+    }
+
+    void Play360Video()
+    {
+        videoPlayer.Play();
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        videoPlayer.Stop();
+        sphereRenderer.material = outStillImageMaterial;
+        outTouchpoints.SetActive(true);
+        HUDClicks(false);
+
+        // Optionally unlock cursor again if interaction is needed
+        // Cursor.lockState = CursorLockMode.None;
+        // Cursor.visible = true;
+    }
+                
+    void HUDClicks(bool state)
+    {
+        // Disable raycast target on HUD elements
+        foreach (Graphic g in HUDCanvas.GetComponentsInChildren<Graphic>())
+        {
+            g.raycastTarget = state;
+        }
+    }
+
+    public void ToStudio()
+    {
+        Debug.Log("To Studio");
+        sphereRenderer.material = inStillImageMaterial;
+        outTouchpoints.SetActive(false);
+        inTouchpoints.SetActive(true);
+    }
+}
